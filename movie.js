9cf636a4cf1669ff7117fdc5b649d98626c1983b
pass <pass@MacBook-Air-2.local>
2025-10-16 15:23:37 -0600

    saving

---
+++ movie.js
@@ -0,0 +1,22 @@
+const movies = [
+    {
+        id: 1,
+        title: "Inception",
+        description: "A thief who steals corporate secrets through the use of dream-sharing technology.",
+        image: "https://image.tmdb.org/t/p/w500/qmDpIHrmpJINaRKAfWQfftjCdyi.jpg",
+        rating: 4.8
+    },
+    {
+        id: 2,
+        title: "The Dark Knight",
+        description: "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham.",
+        image: "https://image.tmdb.org/t/p/w500/qJ2tW6WMUDux911r6m7haRef0WH.jpg",
+        rating: 4.9
+    },
+    {
+        id: 3,
+        title: "Interstellar",
+        description: "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+        image: "https://image.tmdb.org/t/p/w500/rAiYTfKGqDCRIIqo664sY9XZIvQ.jpg",
+        rating: 4.7
+    },
